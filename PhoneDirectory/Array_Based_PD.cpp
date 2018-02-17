6e480c8f7973acddf32cc0d49c6bb8e4e47bf54d
AmonRS <amonsthapit@live.com>
2018-02-17 14:58:35 -0800

    Exercises 1.7 & 1.8

--- PhoneDirectory/Array_Based_PD.cpp
+++ PhoneDirectory/Array_Based_PD.cpp
@@ -113,20 +113,15 @@ void Phone_Directory::save()
 	@return That person's name or an empty string
 	if not in the directory
 	*/
-string Phone_Directory::remove_entry(const string& name) // Exercise 1.7: please complete the remove_entry() method - Ed/Kent
+string Phone_Directory::remove_entry(const string& name) // AMON & KAILEY
 {
-
-	// Hint: you can use the code below to shift names down in the directory to remove the selected entry specified by "index"
-	// for (int i = index; i < size - 1; i++)
-		// the_directory[i] = the_directory[i + 1];
-
 	int index = find(name);
-	if (index > 0) {
-		remove_entry(index);
+	if (index != -1) {
+		std::string number = the_directory[index].get_number();
 		for (int i = index; i < size - 1; i++) {
 			the_directory[i] = the_directory[i + 1];
 		}
-		std::string number = this->
+		size -= 1;
 		return number;
 	}
 	else {
@@ -161,6 +156,10 @@ void Phone_Directory::add(const string& name,
 	// Increment size and add new entry.
 	the_directory[size] = Directory_Entry(name, number);
 	size++;
+}
+
+void Phone_Directory::remove_entry(int index)
+{
 }
 
 /** Create a new array of directory entries with twice the capacity
